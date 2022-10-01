2141116e95ebb5ea25bde6ce234ab0dca28ce810
Shubham <shubhams.15094@gmail.com>
2022-10-01 20:27:54 +0530

    first commit

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>abc/12,UP, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>abc/12, UP</p>
+        <p>987456321</p>
+        <p>info@pay.net</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
